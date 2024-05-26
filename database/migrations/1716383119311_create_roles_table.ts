98e194595c679c5df176e370ea9c3f724178cf1b
EmilienBilly <emilien.billy@gmail.com>
2024-05-26 21:18:12 +0200

    feat: create role table migration

--- database/migrations/1716383119311_create_roles_table.ts
+++ database/migrations/1716383119311_create_roles_table.ts
@@ -1,13 +1,14 @@
 import { BaseSchema } from '@adonisjs/lucid/schema'
 
 export default class extends BaseSchema {
-  protected tableName = 'cda_users'
+  protected tableName = 'roles'
 
   async up() {
     this.schema.createTable(this.tableName, (table) => {
-      table.increments('id').notNullable()
-      table.string('login').nullable()
-      table.string('password').notNullable()
+      table.increments('id')
+
+      table.string('name', 50).notNullable()
+      table.boolean('active').notNullable().defaultTo(true)
     })
   }
 
