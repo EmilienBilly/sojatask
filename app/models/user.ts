32eba291d36305c166ae506b0e324307e1bfe4fe
emilien <emilien.billy@gmail.com>
2024-06-20 16:37:56 +0200

    feat: declare columns in models

--- app/models/user.ts
+++ app/models/user.ts
@@ -14,10 +14,7 @@ export default class User extends compose(BaseModel, AuthFinder) {
   declare id: number
 
   @column()
-  declare fullName: string | null
-
-  @column()
-  declare email: string
+  declare username: string
 
   @column()
   declare password: string
