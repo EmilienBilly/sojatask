bcdaa692d1865bf36ffee22a9d6531071ae0f8b1
emilien <emilien.billy@gmail.com>
2024-07-19 00:22:30 +0200

    feat: display an error if invalid credentials

--- inertia/pages/login.tsx
+++ inertia/pages/login.tsx
@@ -1,11 +1,13 @@
-import { Head } from '@inertiajs/react'
+import { Head, usePage } from '@inertiajs/react'
 import { useForm } from '@inertiajs/react'
+import { SharedProps } from '@adonisjs/inertia/types'
 
 export default function Login() {
   const { data, setData, post, processing, errors } = useForm({
     username: '',
     password: '',
   })
+  const flashError = usePage<SharedProps>().props.flash.all
 
   function submit(event: { preventDefault: () => void }) {
     event.preventDefault()
@@ -18,15 +20,19 @@ export default function Login() {
 
       <div className="container">
         <div className="title">Bienvenue sur Projet_CDA</div>
-
+        {flashError.errorsBag && <div className="alert alert-error">Identifiants incorrects</div>}
         <form onSubmit={submit}>
+          <label htmlFor="username">Nom d'utilisateur</label>
           <input
+            id="username"
             type="text"
             value={data.username}
             onChange={(e) => setData('username', e.target.value)}
           />
           {errors.username && <div>{errors.username}</div>}
+          <label htmlFor="password">Mot de passe</label>
           <input
+            id="password"
             type="password"
             value={data.password}
             onChange={(e) => setData('password', e.target.value)}
