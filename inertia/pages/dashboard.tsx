4191bb52d72154a8a27290d8e7b3d08f53a74e48
emilien <emilien.billy@gmail.com>
2024-07-19 23:27:01 +0200

    chore: test styled-components

--- inertia/pages/dashboard.tsx
+++ inertia/pages/dashboard.tsx
@@ -1,14 +1,21 @@
 import { Head, usePage } from '@inertiajs/react'
 import { SharedProps } from '@adonisjs/inertia/types'
+import styled from 'styled-components'
 
 export default function Dashboard(props: { version: number }) {
   const flash = usePage<SharedProps>().props.flash
+
+  const Flash = styled.div`
+    background-color: seagreen;
+    padding: 10px 10px;
+    color: #f7f8fa;
+  `
   return (
     <>
       <Head title="Dashboard" />
       <div className="container">
-        {flash.success && <div className="alert alert-success">{flash.success}</div>}
-        {flash.error && <div className="alert alert-error">{flash.error}</div>}
+        {flash.success && <Flash>{flash.success}</Flash>}
+        {flash.error && <Flash className="alert alert-error">{flash.error}</Flash>}
         <div className="title">AdonisJS {props.version} x Inertia x React</div>
         <span>
           Learn more about AdonisJS and Inertia.js by visiting the{' '}
