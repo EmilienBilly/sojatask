8d01b9da86c0b386f3a39b9ca0ad04ddd30b24ad
emilien <emilien.billy@gmail.com>
2024-07-19 22:43:37 +0200

    refactor(login): change the way of treating E_INVALID_CREDENTIALS

--- inertia/pages/login.tsx
+++ inertia/pages/login.tsx
@@ -3,11 +3,11 @@ import { useForm } from '@inertiajs/react'
 import { SharedProps } from '@adonisjs/inertia/types'
 
 export default function Login() {
-  const { data, setData, post, processing, errors } = useForm({
+  const { data, setData, post, processing } = useForm({
     username: '',
     password: '',
   })
-  const flashError = usePage<SharedProps>().props.flash.all
+  const error = usePage<SharedProps>().props.flash.error
 
   function submit(event: { preventDefault: () => void }) {
     event.preventDefault()
@@ -20,7 +20,7 @@ export default function Login() {
 
       <div className="container">
         <div className="title">Bienvenue sur Projet_CDA</div>
-        {flashError.errorsBag && <div className="alert alert-error">Identifiants incorrects</div>}
+        {error && <div className="alert alert-error">{error}</div>}
         <form onSubmit={submit}>
           <label htmlFor="username">Nom d'utilisateur</label>
           <input
@@ -29,7 +29,6 @@ export default function Login() {
             value={data.username}
             onChange={(e) => setData('username', e.target.value)}
           />
-          {errors.username && <div>{errors.username}</div>}
           <label htmlFor="password">Mot de passe</label>
           <input
             id="password"
@@ -37,7 +36,6 @@ export default function Login() {
             value={data.password}
             onChange={(e) => setData('password', e.target.value)}
           />
-          {errors.password && <div>{errors.password}</div>}
           <button type="submit" disabled={processing}>
             Login
           </button>
