957ab84b10c24908b36b6a2d8e45d4568cb796e0
emilien <emilien.billy@gmail.com>
2024-07-19 22:43:37 +0200

    refactor(login): change the way of treating E_INVALID_CREDENTIALS

--- app/controllers/auth/login_controller.ts
+++ app/controllers/auth/login_controller.ts
@@ -1,6 +1,7 @@
 import type { HttpContext } from '@adonisjs/core/http'
 import User from '#models/user'
 import { loginValidator } from '#validators/login'
+import { errors as authErrors } from '@adonisjs/auth'
 
 export default class LoginController {
   render({ inertia }: HttpContext) {
@@ -10,10 +11,16 @@ export default class LoginController {
   async login({ request, response, auth, session }: HttpContext) {
     const { username, password } = await request.validateUsing(loginValidator)
 
-    const user = await User.verifyCredentials(username, password)
-
-    await auth.use('web').login(user)
-    session.flash('success', 'Utilisateur connecté')
-    return response.redirect().toPath('/')
+    try {
+      const user = await User.verifyCredentials(username, password)
+      await auth.use('web').login(user)
+      session.flash('success', 'Utilisateur connecté')
+      return response.redirect().toPath('/')
+    } catch (error) {
+      if (error instanceof authErrors.E_INVALID_CREDENTIALS) {
+        session.flash('error', 'Identifiants incorrects')
+      }
+      return response.redirect().back()
+    }
   }
 }
