4279fc06efb6dfd9d6fbc9051441d7289f0701bd
emilien <emilien.billy@gmail.com>
2024-07-19 23:19:41 +0200

    refactor: rename home into dashboard

--- start/routes.ts
+++ start/routes.ts
@@ -3,10 +3,10 @@ import { middleware } from '#start/kernel'
 
 const LogoutController = () => import('#controllers/auth/logout_controller')
 
-const HomeController = () => import('#controllers/home_controller')
+const DashboardController = () => import('#controllers/dashboard_controller')
 const LoginController = () => import('#controllers/auth/login_controller')
 
-router.get('/', [HomeController, 'render']).use(middleware.auth()).as('home.render')
+router.get('/', [DashboardController, 'render']).use(middleware.auth()).as('dashboard.render')
 
 router.get('login', [LoginController, 'render'])
 router.post('login', [LoginController, 'login'])
