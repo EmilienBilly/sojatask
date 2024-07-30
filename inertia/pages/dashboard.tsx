220d43318df621abf8dfb6088286aefcee9e40ea
emilien <emilien.billy@gmail.com>
2024-07-30 19:15:08 +0200

    chore: remove boilerplate text from inertia

--- inertia/pages/dashboard.tsx
+++ inertia/pages/dashboard.tsx
@@ -2,7 +2,7 @@ import { Head, usePage } from '@inertiajs/react'
 import { SharedProps } from '@adonisjs/inertia/types'
 import styled from 'styled-components'
 
-export default function Dashboard(props: { version: number }) {
+export default function Dashboard() {
   const flash = usePage<SharedProps>().props.flash
 
   const Flash = styled.div`
@@ -16,11 +16,7 @@ export default function Dashboard(props: { version: number }) {
       <div className="container">
         {flash.success && <Flash>{flash.success}</Flash>}
         {flash.error && <Flash className="alert alert-error">{flash.error}</Flash>}
-        <div className="title">AdonisJS {props.version} x Inertia x React</div>
-        <span>
-          Learn more about AdonisJS and Inertia.js by visiting the{' '}
-          <a href="https://docs.adonisjs.com/guides/inertia">AdonisJS documentation</a>.
-        </span>
+        <div className="title">Dashboard</div>
       </div>
     </>
   )
