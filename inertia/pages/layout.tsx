d087c9f68edfde92fbbec40e7fb480b4cc25eae6
emilien <emilien.billy@gmail.com>
2024-08-01 19:13:28 +0200

    feat: create sidebar component

--- inertia/pages/layout.tsx
+++ inertia/pages/layout.tsx
@@ -1,15 +1,25 @@
 import { ReactNode } from 'react'
 import Navbar from '~/components/navbar'
+import Sidebar from '~/components/sidebar'
+import styled from 'styled-components'
 
 interface LayoutProps {
   children: ReactNode
 }
 
 const Layout = ({ children }: LayoutProps) => {
+  const Test = styled.div`
+    display: flex;
+    height: 100%;
+    width: 100%;
+  `
   return (
     <>
       <Navbar />
-      <div className="container">{children}</div>
+      <Test>
+        <Sidebar />
+        <div className="container">{children}</div>
+      </Test>
     </>
   )
 }
