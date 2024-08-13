756981d2f6328c495e60b0d3525365bb957994c3
emilien <emilien.billy@gmail.com>
2024-08-13 22:44:43 +0200

    chore: rename the component used for the render method of CreateProjectController

--- app/controllers/create_project_controller.ts
+++ app/controllers/create_project_controller.ts
@@ -2,6 +2,6 @@ import type { HttpContext } from '@adonisjs/core/http'
 
 export default class CreateProjectController {
   render({ inertia }: HttpContext) {
-    return inertia.render('create_project')
+    return inertia.render('createProject')
   }
 }
