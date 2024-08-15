031495d105f4755ed4a5d0143fff077c92a19837
emilien <emilien.billy@gmail.com>
2024-08-15 23:04:03 +0200

    feat(create_project_controller): add create method to make a new project

--- app/controllers/create_project_controller.ts
+++ app/controllers/create_project_controller.ts
@@ -1,7 +1,17 @@
 import type { HttpContext } from '@adonisjs/core/http'
+import { createProjectValidator } from '#validators/project_validator'
+import Project from '#models/project'
+// import Project from '#models/project'
 
 export default class CreateProjectController {
   view({ inertia }: HttpContext) {
     return inertia.render('createProject')
+  }
+
+  async create({ request, auth, session, response }: HttpContext) {
+    const payload = await request.validateUsing(createProjectValidator)
+    await Project.create({ ...payload, createdBy: auth.user!.id })
+    session.flash('success', 'Projet créé')
+    return response.redirect(`/create-project`)
   }
 }
