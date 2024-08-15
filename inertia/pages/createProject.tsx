66497901827d86026a0bd8a3ef9f01ff2c9b1840
emilien <emilien.billy@gmail.com>
2024-08-15 23:05:53 +0200

    feat(createProject): display flash message if the project is successfully created

--- inertia/pages/createProject.tsx
+++ inertia/pages/createProject.tsx
@@ -1,12 +1,23 @@
-import { Head } from '@inertiajs/react'
+import { Head, usePage } from '@inertiajs/react'
 import CreateProjectForm from '~/components/CreateProjectForm'
+import { SharedProps } from '@adonisjs/inertia/types'
+import styled from 'styled-components'
 
 export default function CreateProject() {
+  const flash = usePage<SharedProps>().props.flash
+
+  const Flash = styled.div`
+    background-color: seagreen;
+    padding: 10px 10px;
+    color: #f7f8fa;
+  `
+
   return (
     <>
       <Head title="Create project" />
       <div className="container">
         <div className="title">Nouveau projet</div>
+        {flash.success && <Flash>{flash.success}</Flash>}
         <CreateProjectForm />
       </div>
     </>
