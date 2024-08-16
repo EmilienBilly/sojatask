64035d0c5b0affbb5ff13b8902971331e88c47e7
emilien <emilien.billy@gmail.com>
2024-08-16 23:55:47 +0200

    feat(ProjectsController): add method getUserProjects() to retrieve all projects created by the logged in user

--- app/controllers/projects/projects_controller.ts
+++ app/controllers/projects/projects_controller.ts
@@ -1,9 +1,19 @@
+import { inject } from '@adonisjs/core'
 import type { HttpContext } from '@adonisjs/core/http'
 import { createProjectValidator } from '#validators/project_validator'
 import Project from '#models/project'
+import { ProjectRepository } from '../../repositories/projects_repository.js'
 
+@inject()
 export default class ProjectsController {
-  create({ inertia }: HttpContext) {
+  constructor(private repository: ProjectRepository) {}
+
+  async getUserProjects({ auth }: HttpContext) {
+    const projects = await this.repository.findAllProjectsByUserId(auth.user!.id)
+    console.log(projects)
+  }
+
+  async create({ inertia }: HttpContext) {
     return inertia.render('createProject')
   }
 
