125131e9363f2e4ea4770955a13a1e82b922af5b
emilien <emilien.billy@gmail.com>
2024-08-16 22:47:06 +0200

    chore(controllers): rename CreateProjectController to ProjectsController

--- app/controllers/projects/projects_controller.ts
+++ app/controllers/projects/projects_controller.ts
@@ -1,14 +1,13 @@
 import type { HttpContext } from '@adonisjs/core/http'
 import { createProjectValidator } from '#validators/project_validator'
 import Project from '#models/project'
-// import Project from '#models/project'
 
-export default class CreateProjectController {
-  view({ inertia }: HttpContext) {
+export default class ProjectsController {
+  create({ inertia }: HttpContext) {
     return inertia.render('createProject')
   }
 
-  async create({ request, auth, session, response }: HttpContext) {
+  async store({ request, auth, session, response }: HttpContext) {
     const payload = await request.validateUsing(createProjectValidator)
     await Project.create({ ...payload, createdBy: auth.user!.id })
     session.flash('success', 'Projet créé')
--- start/routes.ts
+++ start/routes.ts
@@ -4,9 +4,9 @@ import { middleware } from '#start/kernel'
 const LogoutController = () => import('#controllers/auth/logout_controller')
 const DashboardController = () => import('#controllers/dashboard_controller')
 const LoginController = () => import('#controllers/auth/login_controller')
-const CreateProjectController = () => import('#controllers/projects/create_project_controller')
+const ProjectsController = () => import('#controllers/projects/projects_controller')
 
-router.get('/', [DashboardController, 'view']).use(middleware.auth()).as('dashboard.render')
+router.get('/', [DashboardController, 'view']).use(middleware.auth())
 
 router.get('login', [LoginController, 'view'])
 router.post('login', [LoginController, 'login'])
@@ -14,12 +14,6 @@ router.post('login', [LoginController, 'login'])
 router.get('logout', [LogoutController, 'view'])
 router.post('logout', [LogoutController, 'logout']).use(middleware.auth())
 
-router
-  .get('create-project', [CreateProjectController, 'view'])
-  .use(middleware.auth())
-  .as('create-project.render')
+router.get('create-project', [ProjectsController, 'create']).use(middleware.auth())
 
-router
-  .post('create-project', [CreateProjectController, 'create'])
-  .use(middleware.auth())
-  .as('create-project.create')
+router.post('create-project', [ProjectsController, 'store']).use(middleware.auth())
