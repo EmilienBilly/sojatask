83cb73bb4f709b96188d7cb9f64d6eab162a9a21
emilien <emilien.billy@gmail.com>
2024-08-18 15:42:03 +0200

    fix: forgot @inject() decorator in ShareUserProjects middleware

--- app/middleware/share_user_projects_middleware.ts
+++ app/middleware/share_user_projects_middleware.ts
@@ -1,7 +1,9 @@
 import type { HttpContext } from '@adonisjs/core/http'
 import type { NextFn } from '@adonisjs/core/types/http'
 import { ProjectRepository } from '../repositories/projects_repository.js'
+import { inject } from '@adonisjs/core'
 
+@inject()
 export default class ShareUserProjectsMiddleware {
   constructor(private repository: ProjectRepository) {}
 
