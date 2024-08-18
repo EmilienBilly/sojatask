d374abb7489f4071d7cfc408e2bd633f4e3b211a
emilien <emilien.billy@gmail.com>
2024-08-18 17:44:31 +0200

    fix: move styled component outside of functions

--- inertia/pages/dashboard.tsx
+++ inertia/pages/dashboard.tsx
@@ -2,14 +2,15 @@ import { Head, usePage } from '@inertiajs/react'
 import { SharedProps } from '@adonisjs/inertia/types'
 import styled from 'styled-components'
 
+const Flash = styled.div`
+  background-color: seagreen;
+  padding: 10px 10px;
+  color: #f7f8fa;
+`
+
 export default function Dashboard() {
   const flash = usePage<SharedProps>().props.flash
 
-  const Flash = styled.div`
-    background-color: seagreen;
-    padding: 10px 10px;
-    color: #f7f8fa;
-  `
   return (
     <>
       <Head title="Dashboard" />
