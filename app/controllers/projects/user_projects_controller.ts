13804db585c23fcbf55cfa8ac7c43741f42ec56a
emilien <emilien.billy@gmail.com>
2024-08-22 10:10:52 +0200

    refactor(controllers): move methods related to user's projects into a new controller named UserProjectsController

---
+++ app/controllers/projects/user_projects_controller.ts
@@ -0,0 +1,13 @@
+import type { HttpContext } from '@adonisjs/core/http'
+import { ProjectRepository } from '../../repositories/projects_repository.js'
+import { inject } from '@adonisjs/core'
+
+@inject()
+export default class UserProjectsController {
+  constructor(private repository: ProjectRepository) {}
+
+  async index({ auth }: HttpContext) {
+    const projects = await this.repository.findAllProjectsByUserId(auth.user!.id)
+    console.log(projects)
+  }
+}
