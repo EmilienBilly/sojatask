767f6399dc7731ba0f15ce7cc976e229d7a93117
emilien <emilien.billy@gmail.com>
2024-09-09 20:25:57 +0200

    fix(fake_seeder): remove unused model data creation

--- database/seeders/fake_seeder.ts
+++ database/seeders/fake_seeder.ts
@@ -1,36 +1,11 @@
 import { BaseSeeder } from '@adonisjs/lucid/seeders'
-import Role from '#models/role'
 import { TaskFactory } from '#database/factories/task_factory'
 import { UserFactory } from '#database/factories/user_factory'
-import TaskStatus from '#models/task_status'
 
 export default class extends BaseSeeder {
   async run() {
     await UserFactory.createMany(5)
 
     await TaskFactory.createMany(5)
-
-    await Role.createMany([
-      {
-        name: 'user',
-        active: true,
-      },
-      {
-        name: 'admin',
-        active: true,
-      },
-    ])
-
-    await TaskStatus.createMany([
-      {
-        name: 'À faire',
-      },
-      {
-        name: 'En cours',
-      },
-      {
-        name: 'Terminé',
-      },
-    ])
   }
 }
