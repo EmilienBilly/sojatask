b08e0d230470fba8324df9ecc3bca34902895301
EmilienBilly <emilien.billy@gmail.com>
2024-09-19 11:04:05 +0200

    feat: add CreateListButton.tsx component

--- inertia/pages/board.tsx
+++ inertia/pages/board.tsx
@@ -1,7 +1,14 @@
 import { InferPageProps } from '@adonisjs/inertia/types'
 import BoardsController from '#controllers/boards/boards_controller'
 import { BoardType } from '~/types/board'
+import styled from 'styled-components'
+import CreateListButton from '~/components/CreateListButton'
 
+const BoardCanva = styled.div`
+  flex-grow: 1;
+  margin-top: 12px;
+  position: relative;
+`
 export default function Board(props: InferPageProps<BoardsController, 'show'>) {
   console.log(props)
   const board: BoardType = props.board
@@ -9,6 +16,9 @@ export default function Board(props: InferPageProps<BoardsController, 'show'>) {
     <>
       <h1>{board.title}</h1>
       <div>{board.description}</div>
+      <BoardCanva>
+        <CreateListButton />
+      </BoardCanva>
     </>
   )
 }
