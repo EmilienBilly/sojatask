0fdba4a779fb01c2aadaf436041c1133f090f541
emilien <emilien.billy@gmail.com>
2024-09-22 17:57:14 +0200

    feat(board.tsx): display board's lists

--- app/controllers/boards/boards_controller.ts
+++ app/controllers/boards/boards_controller.ts
@@ -5,12 +5,13 @@ export default class BoardsController {
   async show({ request, inertia }: HttpContext) {
     console.log('called twice')
     const boardData = await Board.findBy('id', request.param('boardId'))
+    await boardData?.load('lists')
     const board = boardData?.serialize() as {
       id: number
       title: string
       description: string
       projectId: number
     }
-    return inertia.render('board', { board })
+    return inertia.render('board', { board, lists: boardData?.lists })
   }
 }
