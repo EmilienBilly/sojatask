4f3efc2deb9ff18ce54a7a0dc21deedc9e4e63cf
emilien <emilien.billy@gmail.com>
2024-09-23 18:32:46 +0200

    feat: add route, controller and validator to create a task

---
+++ app/controllers/tasks/create_task_controller.ts
@@ -0,0 +1,12 @@
+import type { HttpContext } from '@adonisjs/core/http'
+import Task from '#models/task'
+import { createTaskValidator } from '#validators/task_validator'
+
+export default class CreateTaskController {
+  async handle({ request, response, auth }: HttpContext) {
+    const payload = await request.validateUsing(createTaskValidator)
+
+    await Task.create({ ...payload, createdBy: auth.user!.id })
+    return response.redirect().back()
+  }
+}
