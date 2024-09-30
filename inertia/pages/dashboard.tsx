babf2b1500d718fd7cd182fa9462d399b18906ec
emilien <emilien.billy@gmail.com>
2024-09-30 21:39:07 +0200

    feat: display current date on the dashboard

--- inertia/pages/dashboard.tsx
+++ inertia/pages/dashboard.tsx
@@ -12,16 +12,51 @@ const Flash = styled.div`
   width: 200px;
 `
 
+const Month = styled.p`
+  font-weight: bold;
+  font-size: 18px;
+`
+
+const CurrentDate = styled.div`
+  border: solid 1px #1f1f20;
+  border-radius: 8px;
+  padding: 8px;
+  margin-bottom: 16px;
+`
+
 export default function Dashboard() {
   const flash = usePage<SharedProps>().props.flash
+
+  const getFormattedDate = () => {
+    const today = new Date()
+    const options: Intl.DateTimeFormatOptions = {
+      weekday: 'long',
+      day: 'numeric',
+      month: 'long',
+    }
+    return today.toLocaleDateString('fr-FR', options)
+  }
+
+  const getCurrentMonth = () => {
+    const today = new Date()
+    const options: Intl.DateTimeFormatOptions = {
+      month: 'long',
+    }
+    return new Intl.DateTimeFormat('fr-FR', options).format(today)
+  }
 
   return (
     <>
       <Head title="Dashboard" />
       <Flex $flxCol={true}>
-        {flash.success && <Flash>{flash.success}</Flash>}
         {flash.error && <Flash className="alert alert-error">{flash.error}</Flash>}
         <div className="title">Dashboard</div>
+        <CurrentDate>
+          <Month>{getCurrentMonth().toUpperCase()}</Month>
+          <p>Aujourd'hui, {getFormattedDate()}</p>
+        </CurrentDate>
+        <h3>Projets</h3>
+        <span>Vous avez 3 pojets</span>
       </Flex>
     </>
   )
