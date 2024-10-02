7bba2057921555ed44c6ef1dcc0edaa9b05acf5e
Emilien Billy <emilien.billy@sojadis.com>
2024-10-02 14:21:02 +0200

    feat: create factories for Task, List and Board

--- database/factories/task_factory.ts
+++ database/factories/task_factory.ts
@@ -6,6 +6,7 @@ export const TaskFactory = factory
     return {
       name: faker.word.words(3),
       description: faker.lorem.lines({ min: 1, max: 3 }),
+      archived: 0,
       createdBy: 1,
       listId: undefined,
       deadline: 1,
