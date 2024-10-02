9930ba866791c3a633d84eaa613a6b68377b8baa
Emilien Billy <emilien.billy@sojadis.com>
2024-10-02 16:59:44 +0200

    feat: creatin a functional test

---
+++ tests/unit/boards/show.spec.ts
@@ -0,0 +1,54 @@
+import { test } from '@japa/runner'
+import User from '#models/user'
+import testUtils from '@adonisjs/core/services/test_utils'
+import { BoardFactory } from '#database/factories/board_factory'
+import { ListFactory } from '#database/factories/list_factory'
+import { TaskFactory } from '#database/factories/task_factory'
+
+test.group('BoardsController', (group) => {
+  group.each.setup(() => testUtils.db().withGlobalTransaction())
+
+  test('display board component with the board + lists + task in props', async ({ client }) => {
+    const user = await User.query().where('username', 'emilien.billy').firstOrFail()
+
+    const board = await BoardFactory.create()
+    const list = await ListFactory.merge({ boardId: board.id }).create()
+    const task = await TaskFactory.merge({ listId: list.id }).create()
+
+    const response = await client
+      .get(`/user_projects/${board.projectId}/boards/${board.id}`)
+      .withGuard('web')
+      .loginAs(user)
+      .withInertia()
+
+    response.assertStatus(200)
+
+    response.assertInertiaComponent('board')
+
+    response.assertInertiaPropsContains({
+      board: {
+        id: board.id,
+        title: board.title,
+        description: board.description,
+        projectId: board.projectId,
+        lists: [
+          {
+            id: list.id,
+            title: list.title,
+            boardId: board.id,
+            tasks: [
+              {
+                id: task.id,
+                name: task.name,
+                description: task.description,
+                listId: list.id,
+                archived: task.archived,
+                deadline: task.deadline,
+              },
+            ],
+          },
+        ],
+      },
+    })
+  })
+})
