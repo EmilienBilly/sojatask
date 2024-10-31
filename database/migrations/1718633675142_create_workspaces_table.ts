4bfe445039efa06270b09f2808554c778ee45b93
Emilien Billy <emilien.billy@sojadis.com>
2024-10-31 16:35:53 +0100

    feat: update base migrations to match data structure

--- database/migrations/1718633675142_create_workspaces_table.ts
+++ database/migrations/1718633675142_create_workspaces_table.ts
@@ -1,15 +1,17 @@
 import { BaseSchema } from '@adonisjs/lucid/schema'
 
 export default class extends BaseSchema {
-  protected tableName = 'task_projects'
+  protected tableName = 'task_workspaces'
 
   async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id').notNullable()
 
-      table.string('name').notNullable().unique()
-      table.text('description').notNullable()
+      table.string('title').notNullable().unique()
+      table.text('description').nullable()
       table.integer('created_by').notNullable()
+      table.integer('created_at').notNullable()
+      table.integer('updated_at').nullable()
     })
   }
 
