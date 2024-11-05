82beaf9cd49c06db0921fd10c34a28368796ebfd
Emilien Billy <emilien.billy@sojadis.com>
2024-11-05 11:46:09 +0100

    feat: create contact model

---
+++ app/models/contact.ts
@@ -0,0 +1,15 @@
+import { BaseModel, column } from '@adonisjs/lucid/orm'
+
+export default class Contact extends BaseModel {
+  @column({ isPrimary: true })
+  declare id: number
+
+  @column()
+  declare firstname: string
+
+  @column()
+  declare lastname: string
+
+  @column()
+  declare email: string | null
+}
