fb5865fe0c4694393f0d8dd27138c74b185fe13c
Emilien Billy <emilien.billy@sojadis.com>
2024-11-13 15:29:52 +0100

    feat(BoardDto): add relationships to dto

--- app/dtos/board.ts
+++ app/dtos/board.ts
@@ -1,11 +1,13 @@
 import { BaseModelDto } from '@adocasts.com/dto/base'
 import Board from '#models/board'
+import ListDto from '#dtos/list'
 
 export default class BoardDto extends BaseModelDto {
   declare id: number
   declare title: string
   declare description: string
   declare projectId: number
+  declare lists: ListDto[]
 
   constructor(board?: Board) {
     super()
@@ -15,5 +17,6 @@ export default class BoardDto extends BaseModelDto {
     this.title = board.title
     this.description = board.description
     this.projectId = board.projectId
+    this.lists = ListDto.fromArray(board.lists)
   }
 }
