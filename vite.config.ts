0fd7e98925e8e2b889139b61994eec2452774320
Emilien Billy <emilien.billy@sojadis.com>
2024-11-13 15:28:28 +0100

    feat: install shadcn and tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,24 @@ import { getDirname } from '@adonisjs/core/helpers'
 import inertia from '@adonisjs/inertia/client'
 import react from '@vitejs/plugin-react'
 import adonisjs from '@adonisjs/vite/client'
+import tailwind from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
 
 export default defineConfig({
-  plugins: [inertia({ ssr: { enabled: false } }), react(), adonisjs({ entrypoints: ['inertia/app/app.tsx'], reload: ['resources/views/**/*.edge'] })],
+  plugins: [
+    inertia({ ssr: { enabled: false } }),
+    react(),
+    adonisjs({
+      entrypoints: ['inertia/app/app.tsx'],
+      reload: ['resources/views/**/*.edge'],
+    }),
+  ],
 
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer()],
+    },
+  },
   /**
    * Define aliases for importing modules from
    * your frontend code
