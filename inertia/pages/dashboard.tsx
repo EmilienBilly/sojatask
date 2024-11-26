5b762594936673adcb6bfb5a81d051e3d85bc119
Emilien Billy <emilien.billy@sojadis.com>
2024-11-26 15:51:18 +0100

    refactor: remove old code

--- inertia/pages/dashboard.tsx
+++ inertia/pages/dashboard.tsx
@@ -1,43 +1,17 @@
 import { Head, usePage } from '@inertiajs/react'
 import { SharedProps } from '@adonisjs/inertia/types'
 import styled from 'styled-components'
-import { Flex } from '~/components/utils/Flex'
+import { Flex } from '#inertia/utils/Flex'
 
 export default function Dashboard() {
   const flash = usePage<SharedProps>().props.flash
-
-  const getFormattedDate = () => {
-    const today = new Date()
-    const options: Intl.DateTimeFormatOptions = {
-      weekday: 'long',
-      day: 'numeric',
-      month: 'long',
-    }
-    return today.toLocaleDateString('fr-FR', options)
-  }
-
-  const getCurrentMonth = () => {
-    const today = new Date()
-    const options: Intl.DateTimeFormatOptions = {
-      month: 'long',
-    }
-    return new Intl.DateTimeFormat('fr-FR', options).format(today)
-  }
 
   return (
     <div className="container">
       <Head title="Dashboard" />
       <Flex $flxCol={true} $gap="18px">
         {flash.error && <Flash className="alert alert-error">{flash.error}</Flash>}
-        <div className="title">Dashboard</div>
-        <CurrentDate>
-          <Month>{getCurrentMonth().toUpperCase()}</Month>
-          <p>Aujourd'hui, {getFormattedDate()}</p>
-        </CurrentDate>
-        <div>
-          <h3>Projets</h3>
-          <span>Vous avez 3 pojets</span>
-        </div>
+        <h1>Dashboard</h1>
       </Flex>
     </div>
   )
@@ -50,19 +24,4 @@ const Flash = styled.div`
   padding: 10px 10px;
   color: #f7f8fa;
   width: 200px;
-`
-
-const Month = styled.p`
-  font-weight: bold;
-  font-size: 18px;
-`
-
-const CurrentDate = styled.div`
-  border: solid 1px #1f1f20;
-  border-radius: 3px;
-  padding: 8px;
-  margin-bottom: 16px;
-  @media (min-width: 769px) {
-    width: 40%;
-  }
 `
