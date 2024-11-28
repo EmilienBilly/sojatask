2125841b53647b061a9b1093d491cd8c9ab060d8
Emilien Billy <emilien.billy@sojadis.com>
2024-11-28 16:42:30 +0100

    refactore: redo CreateList.tsx component with shadcn card components

---
+++ inertia/components/CreateTask.tsx
@@ -0,0 +1,61 @@
+import { useState } from 'react'
+import { useForm } from '@inertiajs/react'
+import { Plus, X } from 'lucide-react'
+import { Input } from '#shadcn/input'
+import { Button } from '#shadcn/button'
+import { Card, CardContent, CardFooter } from '#shadcn/card'
+
+type CreateTaskButtonProps = {
+  listId: number
+}
+export default function CreateTask({ listId }: CreateTaskButtonProps) {
+  const [isFormVisible, setIsFormVisible] = useState(false)
+
+  const { data, setData, post, processing, reset } = useForm({
+    title: '',
+    listId: listId,
+  })
+
+  function submit(event: { preventDefault: () => void }) {
+    event.preventDefault()
+    post('/create-task', {
+      onSuccess: () => {
+        setIsFormVisible(false)
+        reset()
+      },
+    })
+  }
+
+  return (
+    <>
+      {isFormVisible ? (
+        <Card>
+          <form onSubmit={submit}>
+            <CardContent className="p-2">
+              <Input
+                type="text"
+                placeholder="Saisissez un nom pour cette tâche"
+                value={data.title}
+                onChange={(e) => setData('title', e.target.value)}
+                required
+              />
+            </CardContent>
+            <CardFooter className="flex justify-between p-2">
+              <Button type="submit" disabled={processing}>
+                Ajouter
+              </Button>
+              <Button variant="ghost" onClick={() => setIsFormVisible(false)}>
+                <X />
+              </Button>
+            </CardFooter>
+          </form>
+        </Card>
+      ) : (
+        <Button variant="outline" className="" onClick={() => setIsFormVisible(true)}>
+          <Plus />
+          Nouvelle tâche
+        </Button>
+      )}
+    </>
+  )
+}
