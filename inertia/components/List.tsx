874ab95766983c04c1abadc9d4cbd793f66ec06d
Emilien Billy <emilien.billy@sojadis.com>
2024-12-05 09:55:08 +0100

    chore: renove pragmatic drag and drop

--- inertia/components/List.tsx
+++ inertia/components/List.tsx
@@ -1,38 +1,17 @@
-import { useEffect, useRef, useState } from 'react'
-import { dropTargetForElements } from '@atlaskit/pragmatic-drag-and-drop/element/adapter'
-import invariant from 'tiny-invariant'
 import Task from '#inertia/Task'
 import { ListType } from '../types/list'
 import { TaskType } from '../types/task'
 import CreateTask from '#inertia/CreateTask'
 
 type ListProps = {
-  listId: number
   tasks: TaskType[]
   list: ListType
 }
 
-export default function List({ listId, tasks, list }: ListProps) {
-  const listRef = useRef(null)
-  const [isDraggedOver, setIsDraggedOver] = useState(false)
-
-  useEffect(() => {
-    const listElement = listRef.current
-    invariant(listElement)
-
-    return dropTargetForElements({
-      element: listElement,
-      onDragEnter: () => setIsDraggedOver(true),
-      onDragLeave: () => setIsDraggedOver(false),
-      onDrop: () => setIsDraggedOver(false),
-      getData: () => ({ listId }),
-      getIsSticky: () => true,
-    })
-  }, [listId])
-
+export default function List({ tasks, list }: ListProps) {
   return (
     <>
-      <div className="flex flex-col min-w-72 border p-4 rounded-md self-start gap-2" ref={listRef}>
+      <div className="flex flex-col min-w-72 border p-4 rounded-md self-start gap-2">
         <h2>{list.title}</h2>
         {tasks?.map((task) => <Task key={task.id} task={task} />)}
         <CreateTask listId={list.id} />
