71213f95ba3b5f35b50029ba4637bd777359e5be
Emilien Billy <emilien.billy@sojadis.com>
2024-12-05 09:55:08 +0100

    chore: renove pragmatic drag and drop

--- inertia/pages/board.tsx
+++ inertia/pages/board.tsx
@@ -1,7 +1,6 @@
 import { InferPageProps } from '@adonisjs/inertia/types'
 import BoardsController from '#controllers/boards/boards_controller'
-import { useCallback, useEffect, useState } from 'react'
-import { monitorForElements } from '@atlaskit/pragmatic-drag-and-drop/element/adapter'
+import { useEffect, useState } from 'react'
 import { Head, router } from '@inertiajs/react'
 import List from '#inertia/List'
 import CreateList from '#inertia/CreateList'
@@ -15,73 +14,11 @@ export default function Board(props: InferPageProps<BoardsController, 'show'>) {
     setLists(props.board.lists)
   }, [props.board.lists])
 
-  const handleDrop = useCallback(
-    ({ source, location }) => {
-      const draggedTaskId = source.data.taskId
-      const [, sourceListRecord] = location.initial.dropTargets
-      const sourceListId = sourceListRecord.data.listId
-
-      // Si la tâche est relâchée en dehors de toute liste
-      if (!location.current.dropTargets.length) {
-        console.log('Aucune destination valide, annulation du déplacement.')
-        return
-      }
-
-      // Vérification de la liste de destination
-      const destinationListRecord = location.current.dropTargets[0]
-      const destinationListId = destinationListRecord.data.listId
-
-      // Vérifier si la destination est différente de la source
-      if (destinationListId && destinationListId !== sourceListId) {
-        moveCard(draggedTaskId, sourceListId, destinationListId)
-        updateTaskListInDB(draggedTaskId, destinationListId)
-      } else {
-        console.log('La tâche a été relâchée dans la même liste, aucune action.')
-      }
-    },
-    [lists]
-  )
-
-  const moveCard = useCallback(
-    (draggedTaskId: number, sourceListId: number, destinationListId: number) => {
-      setLists((prevLists) => {
-        return prevLists.map((prevList) => {
-          if (prevList.id === sourceListId) {
-            // Retirer la tâche de la liste source
-            return {
-              ...prevList,
-              tasks: prevList.tasks.filter((task) => task.id !== draggedTaskId),
-            }
-          }
-          if (prevList.id === destinationListId) {
-            // Ajouter la tâche à la liste destination
-            const draggedTask = lists
-              .find((list) => list.id === sourceListId)
-              ?.tasks.find((task) => task.id === draggedTaskId)
-
-            return {
-              ...prevList,
-              tasks: [...prevList.tasks, { ...draggedTask, listId: destinationListId }],
-            }
-          }
-          return prevList
-        })
-      })
-    },
-    [lists]
-  )
-
   const updateTaskListInDB = (taskId: string, newListId: string) => {
     router.patch(`/tasks/${taskId}/update`, {
       newListId: newListId,
     })
   }
-
-  useEffect(() => {
-    return monitorForElements({
-      onDrop: handleDrop,
-    })
-  }, [handleDrop])
   return (
     <>
       <Head title={board.title} />
@@ -89,7 +26,7 @@ export default function Board(props: InferPageProps<BoardsController, 'show'>) {
       <div className="flex flex-row flex-1 gap-4 p-4 overflow-x-auto">
         {lists?.map((list) => (
           /*TODO: clean up props*/
-          <List key={list.id} listId={list.id} list={list} tasks={list.tasks}></List>
+          <List key={list.id} list={list} tasks={list.tasks}></List>
         ))}
         <CreateList board={board} />
       </div>
