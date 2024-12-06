4d5b57a64f1f7a839f6b056bc2e6202575bef8c1
Emilien Billy <emilien.billy@sojadis.com>
2024-12-06 15:54:16 +0100

    feat: create a custom dialog component to edit a task

--- inertia/components/Task.tsx
+++ inertia/components/Task.tsx
@@ -1,17 +1,21 @@
 import { TaskType } from '../types/task'
 import { Card, CardHeader, CardTitle } from '#shadcn/card'
+import EditTaskDialog from '#inertia/EditTaskDialog'
+import { useState } from 'react'
 
 type TaskCardProps = {
   task: TaskType
 }
 export default function Task({ task }: TaskCardProps) {
+  const [isDialogOpen, setIsDialogOpen] = useState(false)
   return (
     <>
-      <Card className="rounded-md">
+      <Card onClick={() => setIsDialogOpen(!isDialogOpen)} className="rounded-md">
         <CardHeader>
           <CardTitle>{task.title}</CardTitle>
         </CardHeader>
       </Card>
+      <EditTaskDialog open={isDialogOpen} task={task} onOpenChange={setIsDialogOpen} />
     </>
   )
 }
