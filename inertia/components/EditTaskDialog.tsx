722e827511e9198494d1703dd8de0ee41059cc5d
Emilien Billy <emilien.billy@sojadis.com>
2024-12-06 15:54:16 +0100

    feat: create a custom dialog component to edit a task

---
+++ inertia/components/EditTaskDialog.tsx
@@ -0,0 +1,52 @@
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle } from '#shadcn/dialog'
+import { TaskType } from '../types/task'
+import { Label } from '#shadcn/label'
+import { Input } from '#shadcn/input'
+import { useForm } from '@inertiajs/react'
+import { toast } from 'sonner'
+
+type EditTaskDialogProps = {
+  task: TaskType
+  open: boolean
+  onOpenChange: (open: boolean) => void
+}
+
+export default function EditTaskDialog({ task, open, onOpenChange }: EditTaskDialogProps) {
+  const { data, setData, post, processing, reset, errors } = useForm({
+    title: '',
+    description: '',
+  })
+
+  function submit(event: { preventDefault: () => void }) {
+    event.preventDefault()
+    post('/create_board', {
+      onSuccess: () => {
+        reset()
+        toast.success('Tableau créé')
+      },
+    })
+  }
+
+  return (
+    <Dialog open={open} onOpenChange={onOpenChange}>
+      <DialogContent className="w-full">
+        <DialogHeader>
+          <DialogTitle>{task.title}</DialogTitle>
+          <DialogDescription>Modifier la tâche</DialogDescription>
+        </DialogHeader>
+        <form>
+          <div className="grid gap-2">
+            <Label htmlFor="title">Titre</Label>
+            <Input
+              id="title"
+              type="text"
+              value={data.title}
+              onChange={(e) => setData('title', e.target.value)}
+            />
+            {errors.title && <div>{errors.title}</div>}
+          </div>
+        </form>
+      </DialogContent>
+    </Dialog>
+  )
+}
