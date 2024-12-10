0a7c8472a10e2d39b0242942a85d10e8424151d4
Emilien Billy <emilien.billy@sojadis.com>
2024-12-10 16:10:58 +0100

    refactor: change list to column

--- inertia/pages/board.tsx
+++ inertia/pages/board.tsx
@@ -1,14 +1,18 @@
 import { InferPageProps } from '@adonisjs/inertia/types'
 import BoardsController from '#controllers/boards/boards_controller'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { ScrollArea, ScrollBar } from '#shadcn/scroll-area'
 import BoardHeader from '#inertia/BoardHeader'
 import CreateColumn from '#inertia/CreateColumn'
-import List from '#inertia/List'
+import Column from '#inertia/Column'
 import { DndContext } from '@dnd-kit/core'
 
 export default function Board({ board }: InferPageProps<BoardsController, 'show'>) {
   const [columns, setColumns] = useState(board.columns)
+
+  useEffect(() => {
+    setColumns(board.columns)
+  }, [board.columns])
 
   return (
     <>
@@ -17,7 +21,7 @@ export default function Board({ board }: InferPageProps<BoardsController, 'show'
         <DndContext>
           <div className="flex gap-4 flex-row p-4">
             {columns?.map((column) => (
-              <List key={column.id} column={column} tasks={column.tasks} />
+              <Column key={column.id} column={column} tasks={column.tasks} />
             ))}
             <CreateColumn board={board} />
           </div>
