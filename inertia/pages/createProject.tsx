cbe450e2ef11d161a20de31c0b3e6d85695e819c
Emilien Billy <emilien.billy@sojadis.com>
2024-12-10 15:01:18 +0100

    refactor: remove unecessary CreateProjectForm.tsx and move it to the page component

--- inertia/pages/createProject.tsx
+++ inertia/pages/createProject.tsx
@@ -1,28 +1,58 @@
-import { Head, usePage } from '@inertiajs/react'
-import CreateProjectForm from '~/components/CreateProjectForm'
-import { SharedProps } from '@adonisjs/inertia/types'
-import styled from 'styled-components'
-import { Flex } from '~/components/utils/Flex'
+import { Head, useForm } from '@inertiajs/react'
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '#shadcn/card'
+import { Label } from '#shadcn/label'
+import { Input } from '#shadcn/input'
+import { Textarea } from '#shadcn/textarea'
+import { Button } from '#shadcn/button'
+import { toast } from 'sonner'
 
 export default function CreateProject() {
-  const flash = usePage<SharedProps>().props.flash
+  const { data, setData, post, processing, errors } = useForm({
+    title: '',
+    description: '',
+  })
 
-  const Flash = styled.div`
-    background-color: seagreen;
-    padding: 10px 10px;
-    color: #f7f8fa;
-  `
+  function submit(event: { preventDefault: () => void }) {
+    event.preventDefault()
+    post('/workspaces', {
+      onSuccess: () => toast('Espace de travail créé avec succès'),
+    })
+  }
 
   return (
     <>
       <Head title="Create project" />
-      <div className="container">
-        <Flex $flxCol={true} $center={true}>
-          <div className="title">Nouveau projet</div>
-          {flash.success && <Flash>{flash.success}</Flash>}
-          <CreateProjectForm />
-        </Flex>
-      </div>
+      <Card className="mx-auto w-[400px]">
+        <CardHeader>
+          <CardTitle className="text-2xl">Nouveau workspace</CardTitle>
+          <CardDescription>Ajouter un nouveau workspace</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form onSubmit={submit} className="grid gap-4">
+            <div className="grid gap-2">
+              <Label htmlFor="title">Titre</Label>
+              <Input
+                id="title"
+                type="text"
+                value={data.title}
+                onChange={(e) => setData('title', e.target.value)}
+              />
+              {errors.title && <div>{errors.title}</div>}
+            </div>
+            <div className="grid gap-2">
+              <Label htmlFor="description">Description</Label>
+              <Textarea
+                id="description"
+                value={data.description}
+                onChange={(e) => setData('description', e.target.value)}
+              />
+            </div>
+            <Button type="submit" className="w-full" disabled={processing}>
+              Créer
+            </Button>
+          </form>
+        </CardContent>
+      </Card>
     </>
   )
 }
