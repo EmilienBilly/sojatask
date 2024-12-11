1a852d4365ddecbce780c3018a05dfa1507770c8
Emilien Billy <emilien.billy@sojadis.com>
2024-12-11 10:45:38 +0100

    feat: make columns sortable and draggable

--- inertia/components/Column.tsx
+++ inertia/components/Column.tsx
@@ -4,7 +4,8 @@ import { TaskType } from '../types/task'
 import CreateTask from '#inertia/CreateTask'
 import { Card, CardContent, CardHeader } from '#shadcn/card'
 import { ScrollArea } from '#shadcn/scroll-area'
-import { useDroppable } from '@dnd-kit/core'
+import { useSortable } from '@dnd-kit/sortable'
+import { CSS } from '@dnd-kit/utilities'
 
 type ListProps = {
   tasks: TaskType[]
@@ -12,17 +13,30 @@ type ListProps = {
 }
 
 export default function Column({ tasks, column }: ListProps) {
-  const { setNodeRef } = useDroppable({
+  const { attributes, listeners, setNodeRef, transform, transition } = useSortable({
     id: column.id,
   })
 
+  const style = {
+    transform: CSS.Transform.toString(transform),
+    transition,
+  }
+
   return (
-    <Card className="max-h-[720px] w-[350px] max-w-full flex flex-col flex-shrink-0 snap-center">
-      <CardHeader className="p-4 font-semibold border-b-2 text-left flex flex-row space-between items-center">
+    <Card
+      style={style}
+      ref={setNodeRef}
+      className="max-h-[720px] w-[350px] max-w-full flex flex-col flex-shrink-0 snap-center"
+    >
+      <CardHeader
+        {...attributes}
+        {...listeners}
+        className="p-4 font-semibold border-b-2 text-left flex flex-row space-between items-center"
+      >
         {column.title}
       </CardHeader>
       <ScrollArea>
-        <CardContent ref={setNodeRef} className="flex flex-grow flex-col gap-2 p-2">
+        <CardContent className="flex flex-grow flex-col gap-2 p-2">
           {tasks?.map((task) => <Task key={task.id} task={task} />)}
           <CreateTask columnId={column.id} />
         </CardContent>
--- inertia/components/Task.tsx
+++ inertia/components/Task.tsx
@@ -4,7 +4,6 @@ import EditTaskDialog from '#inertia/EditTaskDialog'
 import { useState } from 'react'
 import { GripVertical, Pencil } from 'lucide-react'
 import { Button } from '#shadcn/button'
-import { useDraggable } from '@dnd-kit/core'
 
 type TaskCardProps = {
   task: TaskType
@@ -12,22 +11,9 @@ type TaskCardProps = {
 export default function Task({ task }: TaskCardProps) {
   const [isDialogOpen, setIsDialogOpen] = useState(false)
 
-  const { attributes, listeners, setNodeRef, transform } = useDraggable({
-    id: task.id,
-  })
-
-  const style = transform
-    ? {
-        transform: `translate3d(${transform.x}px, ${transform.y}px, 0)`,
-      }
-    : undefined
   return (
     <>
       <Card
-        ref={setNodeRef}
-        {...listeners}
-        {...attributes}
-        style={style}
         onClick={() => setIsDialogOpen(!isDialogOpen)}
         className="rounded-md hover:bg-hovered cursor-pointer group"
       >
@@ -45,9 +31,7 @@ export default function Task({ task }: TaskCardProps) {
             className="opacity-0 group-hover:opacity-100 transition-opacity duration-200 m-0"
           />
         </CardHeader>
-        <CardContent className="px-3 pt-3 pb-6 text-left whitespace-pre-wrap">
-          {task.title}
-        </CardContent>
+        <CardContent className="px-3 pt-3 pb-6 text-left">{task.title}</CardContent>
       </Card>
       <EditTaskDialog open={isDialogOpen} task={task} onOpenChange={setIsDialogOpen} />
     </>
