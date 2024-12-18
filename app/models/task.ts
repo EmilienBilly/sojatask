3408ef6e5a3497fadebef5f99d8e39e434094966
Emilien Billy <emilien.billy@sojadis.com>
2024-12-18 15:43:43 +0100

    feat: add order column to columns table

--- app/models/task.ts
+++ app/models/task.ts
@@ -1,5 +1,5 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column } from '@adonisjs/lucid/orm'
+import { BaseModel, beforeCreate, column } from '@adonisjs/lucid/orm'
 
 export default class Task extends BaseModel {
   static table = 'task_tasks'
@@ -30,4 +30,17 @@ export default class Task extends BaseModel {
 
   @column()
   declare columnId: number
+
+  @column()
+  declare order: number
+
+  @beforeCreate()
+  static async setOrder(newTask: Task) {
+    const maxOrderColumn = await Task.query()
+      .where('column_id', newTask.columnId)
+      .orderBy('order', 'desc')
+      .first()
+
+    newTask.order = (maxOrderColumn?.order ?? -1) + 1
+  }
 }
