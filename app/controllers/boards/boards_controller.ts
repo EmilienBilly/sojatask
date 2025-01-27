5559db7c4eb21f53e309a24156af50271f5b7624
emilien <emilien.billy@gmail.com>
2025-01-27 16:22:11 +0100

    feat: lazy load only the task when opening it and not the entire board like the initial page

--- app/controllers/boards/boards_controller.ts
+++ app/controllers/boards/boards_controller.ts
@@ -1,15 +1,25 @@
 import { HttpContext } from '@adonisjs/core/http'
 import GetBoardWithColumnsAndTasks from '#actions/boards/get_board_with_columns_and_tasks'
 import BoardDto from '#dtos/board'
+import TaskDto from '#dtos/task'
+import Task from '#models/task'
 
 export default class BoardsController {
   async show({ request, inertia }: HttpContext) {
     const boardId = request.param('boardId')
-
-    const { board } = await GetBoardWithColumnsAndTasks.handle(boardId)
+    const taskId = request.param('taskId')
 
     return inertia.render('board', {
-      board: new BoardDto(board),
+      board: await (async () => {
+        const { board } = await GetBoardWithColumnsAndTasks.handle(boardId)
+        return new BoardDto(board)
+      })(),
+      task: async () => {
+        if (taskId) {
+          const task = await Task.findOrFail(taskId)
+          return new TaskDto(task)
+        }
+      },
     })
   }
 }
