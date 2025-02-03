0d9373122d360d9753b149ab919f9a72180da3ee
Emilien Billy <emilien.billy@sojadis.com>
2025-02-03 15:10:09 +0100

    feat(TaskController): add createSubtask method that uses CreateSubtask action

--- app/controllers/tasks/tasks_controller.ts
+++ app/controllers/tasks/tasks_controller.ts
@@ -1,7 +1,12 @@
 import type { HttpContext } from '@adonisjs/core/http'
-import { taskOrderValidator, taskValidator } from '#validators/task_validator'
+import {
+  createSubtaskValidator,
+  taskOrderValidator,
+  taskValidator,
+} from '#validators/task_validator'
 import UpdateTaskOrder from '#actions/tasks/update_task_order'
 import UpdateTask from '#actions/tasks/update_task'
+import CreateSubtask from '#actions/tasks/create_subtask'
 
 export default class TaskController {
   async order({ params, request, response }: HttpContext) {
@@ -18,6 +23,16 @@ export default class TaskController {
     const data = await request.validateUsing(taskValidator)
     await UpdateTask.handle({
       id: params.id,
+      data,
+    })
+
+    return response.redirect().back()
+  }
+
+  async createSubtask({ params, request, response }: HttpContext) {
+    const data = await request.validateUsing(createSubtaskValidator)
+    await CreateSubtask.handle({
+      parentId: params.id,
       data,
     })
 
