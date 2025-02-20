ca232d2603597a768c43bbbac73089005506d409
Emilien Billy <emilien.billy@sojadis.com>
2025-02-20 10:29:10 +0100

    feat(WorkspaceDto): add users property to workspace data transfer object

--- app/dtos/workspace.ts
+++ app/dtos/workspace.ts
@@ -1,13 +1,14 @@
 import { BaseModelDto } from '@adocasts.com/dto/base'
 import Workspace from '#models/workspace'
 import BoardDto from '#dtos/board'
-
+import UserDto from '#dtos/user'
 export default class WorkspaceDto extends BaseModelDto {
   declare id: number
   declare title: string
   declare description: string
   declare createdBy: number
   declare boards: BoardDto[]
+  declare users: UserDto[]
 
   constructor(workspace?: Workspace) {
     super()
@@ -18,5 +19,6 @@ export default class WorkspaceDto extends BaseModelDto {
     this.description = workspace.description
     this.createdBy = workspace.createdBy
     this.boards = BoardDto.fromArray(workspace.boards)
+    this.users = UserDto.fromArray(workspace.users)
   }
 }
